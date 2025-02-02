6350549b745c74ec3be3d7a5905ff3ce7bff46e7
Fayçal Yacouba Omarou <90855308+LeGrandF38@users.noreply.github.com>
2025-02-02 19:41:27 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,22 @@ if [ -v PASSWORD_FILE ]; then
     PASSWORD="$(< $PASSWORD_FILE)"
 fi
 
-# set the postgres database host, port, user and password according to the environment
-# and pass them as arguments to the odoo process if not present in the config file
+# Configurations de base
 : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
 : ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
 : ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
 
+# Vérification du fichier de configuration
+ODOO_RC=${ODOO_RC:-/etc/odoo/odoo.conf}
+
 DB_ARGS=()
 function check_config() {
     param="$1"
     value="$2"
-    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
-        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
-    fi;
+    if [ -f "$ODOO_RC" ] && grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC"; then
+        value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" | cut -d " " -f3 | tr -d '["\n\r]')
+    fi
     DB_ARGS+=("--${param}")
     DB_ARGS+=("${value}")
 }
@@ -28,22 +30,24 @@ check_config "db_port" "$PORT"
 check_config "db_user" "$USER"
 check_config "db_password" "$PASSWORD"
 
+# Logique principale
 case "$1" in
     -- | odoo)
         shift
-        if [[ "$1" == "scaffold" ]] ; then
+        if [[ "$1" == "scaffold" ]]; then
             exec odoo "$@"
         else
-            wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+            /usr/local/bin/wait-for-psql.py "${DB_ARGS[@]}" --timeout=30
             exec odoo "$@" "${DB_ARGS[@]}"
         fi
         ;;
     -*)
-        wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+        /usr/local/bin/wait-for-psql.py "${DB_ARGS[@]}" --timeout=30
         exec odoo "$@" "${DB_ARGS[@]}"
         ;;
     *)
         exec "$@"
+        ;;
 esac
 
 exit 1
