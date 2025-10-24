a3b839771d0394239cee30954c816383a252c45c
LeGrandF38 <faycalyacoubaprog@gmail.com>
2025-10-24 11:44:55 +0100

    Bump version to force module update

--- addons/gaoh_conseil/__manifest__.py
+++ addons/gaoh_conseil/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Gaoh Conseil - Gestion des Investisseurs',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'category': 'Sales/CRM',
     'summary': 'Gestion des investisseurs, placements et investissements immobiliers',
     'description': """
